c4a3af3bb9f8e27b037a7e9cfa5442c43fb2690e
ashty-drone <77475908+ashty-drone@users.noreply.github.com>
2022-03-25 09:39:30 +0530

    Update okteto-up.py

--- setup/okteto-up.py
+++ setup/okteto-up.py
@@ -4,7 +4,7 @@ from sys import exit
 token = os.environ.get("OKTETO_TOKEN")
 if not token: exit("#"*10 + "\nSet OKTETO_TOKEN!!" + "#"*10)
 
-OKTETO = dict()
+Deployed = False
   
 def okteto_up():
   from logging import getLogger
@@ -22,9 +22,7 @@ okteto push
 from datetime import datetime
 start_time = datetime.now()
 
-OKTETO.update({"deployed"}: False})
-
-while not OKTETO["deployed"]:
+while not Deployed:
   curr_time = datetime.now()
   uptime = curr_time - start_time
-  if uptime.seconds >= 120: OKTETO.update({"deployed"}: True}); okteto_up()
+  if uptime.seconds >= 120: Deployed = True; okteto_up()
